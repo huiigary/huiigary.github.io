a2b96c9a5245238eaf504fde5c03ad1a1e1a82c5
Gary Hui <t9z9a@ugrad.cs.ubc.ca>
2020-09-03 15:44:16 -0700

    Updates

---
+++ precache-manifest.14784bb5ffff51fc5f0b44c564a1f71d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdbbc3458ed994c0630fd4b2373c8cd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46a21e06525fb819dc3e",
+    "url": "/static/css/main.490ef9c3.chunk.css"
+  },
+  {
+    "revision": "0c6df874626fc47d6780",
+    "url": "/static/js/2.d710e973.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d710e973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46a21e06525fb819dc3e",
+    "url": "/static/js/main.5cd67c42.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "09662bc35bd89084c4dc8805acea3fca",
+    "url": "/static/media/resume.09662bc3.pdf"
+  }
+]);
